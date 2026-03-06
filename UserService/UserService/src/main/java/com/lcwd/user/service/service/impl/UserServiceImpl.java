c892ae502630ee14d16446ca5553a6f11733c8b3
asheesh859 <ak2305@checkboxtechnology.com>
2026-03-06 15:43:40 +0530

    adding hotels in users services

--- UserService/UserService/src/main/java/com/lcwd/user/service/service/impl/UserServiceImpl.java
+++ UserService/UserService/src/main/java/com/lcwd/user/service/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.lcwd.user.service.service.impl;
 
+import com.lcwd.user.service.entity.Hotel;
 import com.lcwd.user.service.entity.Rating;
 import com.lcwd.user.service.entity.User;
 import com.lcwd.user.service.repository.UserRepository;
@@ -7,12 +8,15 @@ import com.lcwd.user.service.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -22,6 +26,7 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private RestTemplate restTemplate;
+
 
     Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
@@ -49,10 +54,18 @@ public class UserServiceImpl implements UserService {
       //fetch ratting from ratting service
     //http://localhost:8083/ratings/users/2dd396a5-85f0-4be4-86c5-150998a96bad
 
-       ArrayList<Rating> ratingOfUser =  restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserID(), ArrayList.class);
+        Rating[] ratingOfUser =  restTemplate.getForObject("http://RATING-SERVICE/ratings/users/"+user.getUserID(),Rating[].class);
        logger.info("{} ",ratingOfUser);
 
-       user.setRating(ratingOfUser);
-        return user;
+        List<Rating> ratings = Arrays.stream(ratingOfUser).toList();
+
+        List<Rating> listOfRating = ratings.stream().map(rating ->{
+
+            ResponseEntity<Hotel> hotel = restTemplate.getForEntity("http://HOTEL-SERVICE/hotels/"+rating.getHotelId(), Hotel.class);
+                rating.setHotel(hotel.getBody());
+               return rating;
+        }).collect(Collectors.toList());
+         user.setRating(listOfRating);
+            return user;
     }
 }
